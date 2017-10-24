bc4ac32ebf9b0d845202f808a5e0fe0549046cd3
Kristen Kulha and LDMacKrell <kristen@>
2017-10-24 10:47:58 -0700

    Update app to allow for editing tasks.

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,11 +4,24 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <div class="container">
-  <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
-  <h3>{{currentFocus}}</h3>
-  <ul>
-    <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}}  <button (click)="editTask()">Edit!</button></li>
-  </ul>
+    <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+    <h3>{{currentFocus}}</h3>
+    <ul>
+      <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}}  <button (click)="editTask(currentTask)">Edit!</button></li>
+    </ul>
+    <hr>
+    <div>
+      <h3>{{selectedTask.description}}</h3>
+      <p>Task Complete? {{selectedTask.done}}</p>
+      <h3>Edit Task</h3>
+      <label>Enter Task Description:</label>
+      <input [(ngModel)]="selectedTask.description">
+      <label>Enter Task Priority (1-3):</label>
+      <br>
+      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
+      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
+      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)<br>
+    </div>
   </div>
   `
 })
@@ -22,11 +35,11 @@ export class AppComponent {
   tasks: Task[] = [
     new Task('Finish weekend Angular homework for Epicodus course', 3),
     new Task('Begin brainstorming possible JavaScript group projects', 2),
-    new Task('Add README file to last few Angular repos on GitHub', 2)
-  ];
+    new Task('Add README file to last few Angular repos on GitHub', 2)  ];
+  selectedTask: Task = this.tasks[0];
 
-  editTask() {
-    alert("You just requested to edit a Task!");
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
   }
 
   isDone(clickedTask: Task) {
